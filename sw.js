d4028de91c8cb94c8c17821f9eeaf69988ebd39d
jpORIG <87240317+jpORIG@users.noreply.github.com>
2025-03-28 11:41:22 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+// sw.js
+self.addEventListener('install', function(event) {
+  console.log('Service Worker: Installed');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker: Activated');
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('Service Worker: Fetching', event.request.url);
+  event.respondWith(fetch(event.request));
+});
