c6794db2446ba30136e5a67e0d02f36e94fd694f
jpORIG <87240317+jpORIG@users.noreply.github.com>
2025-07-17 15:53:40 +0200

    pwa

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'klavier-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  // ggf. weitere Assets wie Sounds, Styles, etc.
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
